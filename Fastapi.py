826a0bd6edcd57b0b9bac1c3720bc425fda2400c
Maurice1018 <a85633pp@gmail.com>
2024-12-02 19:38:52 +0800

    20241202Finish_api

---
+++ Fastapi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import json
+
+
+# In[2]:
+
+
+class Item(BaseModel):
+    運動中心: str
+    運動種類: str
+    場地: str
+    日期: str
+    時間: str
+
+app = FastAPI()
+@app.get("/items/", response_model=List[Item])
+async def get_items():
+    with open('badminton.json' , 'r' , encoding = 'utf-8') as f:
+        items = json.load(f)
+    return items
+
---
+++ 爬羽球場地.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import time
+import datetime
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+# In[2]:
+
+
+#得到登入session，把原本的刪除串改後放回去
+def change_id_cookie(driver , id_session):
+    cookie = driver.get_cookie('ASP.NET_SessionId')
+    if not cookie:
+        print('No Cookie detect.')
+        return
+    elif cookie['value'] == id_session:
+        return
+    cookie['value'] = id_session
+    driver.delete_cookie('ASP.NET_SessionId')
+    driver.add_cookie(cookie)
+
+def fetch_badminton():
+    t = time.time()
+    #得到明天的日期格式
+    date = datetime.datetime.today() + datetime.timedelta(days = 1)
+    date = date.strftime('%Y/%m/%d')
+    
+    #建立儲存資料的df
+    df = pd.DataFrame(columns = ['運動中心' , '運動種類' , '場地' , '日期' , '時間'])
+    
+    #給定基本參數
+    #website的aspx編碼
+    websites = {'南港':'02',
+                '汐止':'09',
+                '內湖':'12'}
+    
+    #D分為1早上 2下午 3晚上
+    Ds = [1,2,3]
+
+    #讀取id_session，怎麼加密保護還沒想好
+    with open('id_session.txt') as f:
+        id_session = f.readline()
+        f.close()
+    
+    #給瀏覽器的options增加user_agent的訊息
+    options = ChromeOptions() 
+    options.add_argument('--headless')
+    user_agent = "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    options.add_argument(user_agent)
+    
+    driver = webdriver.Chrome(options = options, service=Service())
+    
+    #df的index
+    i = 0
+    
+    for k , v in websites.items():
+        
+        url = f'https://scr.cyc.org.tw/tp{v}.aspx?module=ind&files=ind'
+        driver.get(url)
+        
+        change_id_cookie(driver)
+        time.sleep(0.5)
+        D = 1 #先給定1要有個初始網站爬所有可訂日期
+        url_book = f'https://scr.cyc.org.tw/tp{v}.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D={date}&D2={D}'
+        driver.get(url_book)
+
+        sport_type = driver.find_element(by=By.ID , value = 'ContentPlaceHolder1_Step2_SportType_lab').text
+
+        dates_list = []
+        for d in driver.find_element(By.ID , value = 'ContentPlaceHolder1_Date_Step2_lab').find_elements(By.CSS_SELECTOR , value = 'option'):
+            dates_list.append(d.get_property('value'))
+
+        for d in dates_list:
+
+            for D in Ds:
+                url_book = f'https://scr.cyc.org.tw/tp{v}.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D={d}&D2={D}'
+                driver.get(url_book)
+                book_table = driver.find_element(By.XPATH , value = '/html/body/div/table[1]/tbody/tr[3]/td/div/form/table/tbody/tr/td/span/div/table/tbody/tr[2]/td/span/table')
+                for _ in book_table.find_elements(By.CSS_SELECTOR , value = 'img'):
+                    if 'confirm' in _.get_attribute('onclick'):
+                        available = _.get_attribute('onclick').split("'")[1].split('「')[1].split('」')[0]
+                        time_ = available.split()[-1]
+                        area = ''.join(available.split()[:-1])
+                        df.loc[i] = [k , sport_type , area , d , time_]
+                        i+=1
+
+    driver.close()
+    print(f'Spend : {time.time()-t:.2f}')
+    
+    date = date.replace('/','')
+    df.to_csv(f'./歷史資料/{date}_羽球.csv', index=False) #存歷史資料用
+    df.to_excel(f'./歷史資料/{date}_羽球.xlsx', index=False) #存歷史資料用
+    df.to_json('badminton.json' , orient='records' , force_ascii=False) #API用
+    
+if __name__ == '__main__':
+    fetch_badminton()
+
+
+# In[ ]:
+
+
+
+
