ff3c299001dc31cf08421c1cd79544dd26a74793
Maurice1018 <a85633pp@gmail.com>
2024-12-05 22:46:16 +0800

    20241205_Finish_Fetch_page

--- Fastapi_html.py
+++ Fastapi_html.py
@@ -38,7 +38,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # 從檔案讀取資料的 API
-@app.get("/data", response_model=ItemResponse)
+@app.get("/data/", response_model=ItemResponse)
 async def get_items():
     with open("badminton.json", "r", encoding="utf-8") as f:
         items = json.load(f)
@@ -49,17 +49,28 @@ async def get_items():
 @app.get("/api/refresh-session")
 async def refresh_session():
     try:
-        result = refresh_sessionID()
-        if result:
-            return {"message": f"Session refreshed successfully. {result}"}
-        else:
-            return {"message": "Session ID expired."}
+        refresh_sessionID()
+        return {"message": "Session refreshed successfully"}
     except Exception as e:
         return {"message": f"Failed to refresh session: {str(e)}"}
+
+@app.get("/api/fetch-data")
+async def fetch_data():
+    try:
+        t= time.time()
+        fetch_badminton()
+        spend_time = round(time.time() - t , 2)
+        return {"message": f"Fetch Data successfully. Spend_time : {spend_time} s"}
+    except Exception as e:
+        return {"message": f"Failed to Fetch Data: {str(e)}"}
 
 @app.get("/refresh")
 async def refresh_page(request: Request):
     return templates.TemplateResponse("refresh.html", {"request": request})
+
+@app.get("/fetch")
+async def fetch_page(request: Request):
+    return templates.TemplateResponse("fetch.html", {"request": request})
 
 # 提供運動資料給前端
 @app.get("/")
---
+++ static/fetch.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const refreshButton = document.getElementById("fetchButton");
+    const resultElement = document.getElementById("result");
+
+    refreshButton.addEventListener("click", async () => {
+        resultElement.textContent = "Refreshing...";
+        try {
+            const response = await fetch('/api/fetch-data');
+            const result = await response.json();
+            resultElement.textContent = `Message: ${result.message}`;
+        } catch (error) {
+            resultElement.textContent = `Error: ${error.message}`;
+        }
+    });
+});
